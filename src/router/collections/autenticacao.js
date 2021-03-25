ad2e28a6760db0ad1b31fbc2ba4df72cd43521ff
Leandro Faria <leandro@leandrofaria.com>
2021-03-24 21:27:53 -0300

    Update autenticacao.js

--- src/router/collections/autenticacao.js
+++ src/router/collections/autenticacao.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.post('/login', function (req, res) {
     // Não faremos autenticação em banco de dados no momento
     // Apenas uma simples validação a titulo de implementacao de login
-    
+
     const { usuario, senha } = req.body;
     if (usuario === 'admin' && senha === 'admin') {
         let resposta = new ApiResponse("sucesso", "Login realizado com sucesso como administrador.", "jwt token vai aqui");
