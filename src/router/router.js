e0f28a0dd24c08b1b0f03d49a0555d991ac60d49
Carlos Daniel <carlos.daniel@devsquad.email>
2021-03-25 21:13:00 -0300

    removing yarn

--- src/router/router.js
+++ src/router/router.js
@@ -1,16 +1,18 @@
-module.exports = function (app) {
-  app.get('/', (req, res, next) => {
+const collections = require('./collections');
+
+module.exports = (app) => {
+  app.get('/', (req, res) => {
     res.status(200).json({ status: 'Backend funcional, API está OK.' });
   });
 
   // Carrega as rotas de acesso do Módulo de Usuários
-  app.use('/usuario', require('./collections/usuario'));
+  app.use('/usuario', collections.usuario);
 
   // Carrega as rotas de acesso do Módulo de Autenticação
-  app.use('/auth', require('./collections/autenticacao'));
+  app.use('/auth', collections.autenticacao);
 
   // Catch all
-  app.use('*', (req, res, next) => {
+  app.use('*', (req, res) => {
     res.status(404).json({ erro: 'O caminho não existe.' });
   });
 };
