c0fae4ec040124cd13959371ae63b3b6f3cfd82d
Leandro Faria <leandro@leandrofaria.com>
2021-05-03 18:55:11 -0300

    Delete src directory

--- src/controllers/index.js
+++
@@ -1,4 +0,0 @@
-export default {
-  default: (req, res) => res.sendStatus(200),
-  notFound: (req, res) => res.sendStatus(404),
-};
--- src/models/index.js
+++
@@ -1,7 +0,0 @@
-import Atendente from './atendente';
-import Consulta from './consulta';
-import Especialidade from './especialidade';
-import Medico from './medico';
-import Paciente from './paciente';
-
-export { Atendente, Consulta, Especialidade, Medico, Paciente };
