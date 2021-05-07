8bde439a69197261ee5da9d37854343606a55e72
Leandro Faria <leandro@leandrofaria.com>
2021-05-06 21:13:09 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
