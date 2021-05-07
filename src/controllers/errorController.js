f256a6328ddfcec001ade22f662aa7410a7f5895
Leandro Faria <leandro@leandrofaria.com>
2021-05-06 23:50:19 -0300

    Update errorController.js

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -43,6 +43,13 @@ module.exports = (err, req, res, next) => {
     });
   }
 
+  if (err.name === 'JsonWebTokenError') {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Invalid authentication data.',
+    });
+  }
+
   return res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
