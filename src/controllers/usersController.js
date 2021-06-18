778323f85897ba45db0144b92abbcf8728b104d2
Leandro Faria <leandro@leandrofaria.com>
2021-06-17 22:10:46 -0300

    Rotas aninhadas das especialidades

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -7,3 +7,90 @@ exports.retrieveOne = factory.retrieveOne(User);
 // DO NOT USE UPDATE USER TO CHANGE THE USER PASSWORD
 exports.update = factory.update(User);
 exports.delete = factory.delete(User);
+
+exports.adicionaEspecialidade = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userid);
+
+    if (!user) {
+      return res.status(200).json({
+        status: 'fail',
+        message: 'User not found.',
+      });
+    }
+
+    if (user.role !== 'medico') {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Apenas medicos possuem especialidade.',
+      });
+    }
+
+    user.details.especialidade = req.body.especialidade;
+
+    await user.save();
+
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        user: user,
+      },
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+exports.listaEspecialidade = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userid);
+
+    if (!user) {
+      return res.status(200).json({
+        status: 'fail',
+        message: 'User not found.',
+      });
+    }
+
+    if (user.role !== 'medico') {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'O usuario nao eh medico e portanto nao possui especialidade.',
+      });
+    }
+
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        especialidade: user.details.especialidade,
+      },
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+exports.removeEspecialidade = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userid);
+
+    if (!user) {
+      return res.status(200).json({
+        status: 'fail',
+        message: 'User not found.',
+      });
+    }
+
+    user.details.especialidade = undefined;
+    await user.save();
+
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        user: user,
+      },
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -13,4 +13,13 @@ router.get('/:id', usersController.retrieveOne);
 router.patch('/:id', usersController.update);
 router.delete('/:id', usersController.delete);
 
+// Metodos para definir e exibir especialidade de um usuario (se ele for medico)
+// Como cada medico possui apenas uma especialidade, as rotas e metodos abaixo
+// sao mais simples do que os trabalhados em sala.
+router.post('/:userid/especialidade', usersController.adicionaEspecialidade);
+router.get('/:userid/especialidade', usersController.listaEspecialidade);
+// No caso de uma unica especialidade, atualizar eh o mesmo que criar uma nova, pois a antiga eh sobrescrita
+router.patch('/:userid/especialidade', usersController.adicionaEspecialidade);
+router.delete('/:userid/especialidade', usersController.removeEspecialidade);
+
 module.exports = router;
